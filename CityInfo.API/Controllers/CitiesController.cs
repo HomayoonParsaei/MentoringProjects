8e800a74ff9e613b9c20d275c310a2aa4ba4a0d2
Homayoon Parsaei <Homayoon.parsaei@Gmail.com>
2025-01-05 11:00:58 -0600

    Add CityInfo.API

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+
+            //return new JsonResult(
+            //    new List<object>
+            //    {
+            //          new {id = 1,Name ="New York City"},
+            //        new {id = 2,Name ="Antwerp"}
+            //    });
+        }
+    }
+          
+}
